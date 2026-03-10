d4d31175b8c39cc81b6e774e348aa039c7f9cefe
Lucas Athanasio <lucasbuenodeandrade@gmail.com>
2026-03-10 15:55:10 -0300

    novos componentes criados: benefits, collaboration e services. adição desses componentes no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Button from "./components/Button";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
 
 const App = () => {
   return (
     <>
-      <div className="pt-4.[75rem] lg:pt-[5.25rem] overflow-hidden">
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden bg-n-8 min-h-screen">
         <Header />
         <Hero />
+        <Benefits />
+        <Collaboration />
+        <Services />
       </div>
       <ButtonGradient />
     </>
