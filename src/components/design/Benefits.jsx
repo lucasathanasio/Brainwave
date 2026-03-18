4cf718a7094071f05a9b5b6f1202f3a08b84a8d1
Lucas Athanasio <lucasbuenodeandrade@gmail.com>
2026-03-18 11:07:32 -0300

    resolvendo o problema do brilho de fundo na seção Benefits não estar aparecendo

--- src/components/design/Benefits.jsx
+++ src/components/design/Benefits.jsx
@@ -1,5 +1,10 @@
 export const GradientLight = () => {
   return (
-    <div className="absolute top-0 left-1/4 w-full aspect-square bg-radial-gradient from-[#28206C] to-[#28206C]/0 to-70% pointer-events-none" />
+    <div
+      className="absolute top-0 left-1/4 w-full aspect-square pointer-events-none"
+      style={{
+        background: "radial-gradient(circle, #28206C 0%, transparent 70%)",
+      }}
+    />
   );
 };
